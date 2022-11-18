6bc30b6c3760229af93fdc8f91a18c2f866ea4a1
Yurij M <mokruy9999@gmail.com>
2022-11-18 23:27:47 +0100

    REMA-3, add - composer require --dev symfony/maker-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
