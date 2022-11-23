94dec53a831913d1e7ac76824217590dd5f3dec0
Yurij M <mokruy9999@gmail.com>
2022-11-23 22:53:53 +0100

    REMA-9, remove unnecessary strings from the main page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
